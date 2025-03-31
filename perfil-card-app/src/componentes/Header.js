9208bdf05720cfaa634e6c4d594ff8fe25f15e63
Arthur Alonso <72287206+Alieinstein2008@users.noreply.github.com>
2025-03-31 19:57:54 -0300

    :bug: Erros corrigidos

--- perfil-card-app/src/componentes/Header.js
+++ perfil-card-app/src/componentes/Header.js
@@ -1,12 +1,12 @@
-export default function Header() {
+export default function Header(props) {
   return (
     <header>
       <div>
         <img
-          src="https://avatars.githubusercontent.com/u/72287206?s=400&u=9d4aee77311f3fefca13057044bfc78800774589&v=4"
-          alt="Foto de Arthur Alonso Desenvolvedor de Sistemas"
+          src={props.PerfilImageUrl}
+          alt={props.PerfilImageAlt}
         />
-        <h1>Systems Developer</h1>
+        <h1>{props.PerfilTitle}</h1>
       </div>
     </header>
   );
